f0398fc2f9fcca4b6d55947609ded38d0f0275a3
Neto1919 <netomengao19@gmail.com>
2025-03-13 22:24:32 -0300

    Atulização 13/03/2025

---
+++ treina_python/treino.py
@@ -0,0 +1,2 @@
+# Este é um arquivo de treino para Python
+print("Olá, bem-vindo ao treino de Python!")
---
+++ treino.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+codigo = 10
+salario = 1500.00
+nome = 'Neto'
+situação = True
+
+tipo = type(salario)
+
+print(salario)
+print(tipo)
+
+print("Codigo: ", codigo, "Nome: ", nome, "O salario atual é de ", salario)
+
+print("Codigo: "+ str(codigo)+ "Nome: "+ nome+ "O salario atual é de "+ str(salario))
